6452c9e9be4498b3db425c396603bc315cd85d03
Promise Johnson <promisej@phreetech.com>
2023-11-24 16:49:12 +0100

    update on the first

--- server.js
+++ server.js
@@ -11,7 +11,6 @@ app.get('*', (req, res)=>{
     res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'))
 })
 
-
 app.listen(port, ()=>{
     console.log(`Server running on port ${port}`)
 })
